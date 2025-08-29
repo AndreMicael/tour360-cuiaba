77da5a8d9d106b2bac698a64d7632b6b9f26c9ec
Andre <andremicael@gmail.com>
2025-08-29 18:21:16 -0400

    Enviando arquivos

---
+++ app-files/data.js
@@ -0,0 +1,328 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-praa-da-repblica",
+      "name": "Praça da República",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.585946798078208,
+        "pitch": -0.034771304993938656,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0379323234666176,
+          "pitch": -0.1380129629139688,
+          "rotation": 0,
+          "target": "1-frente-da-igreja"
+        },
+        {
+          "yaw": -2.2356760669720455,
+          "pitch": -0.24140475539219253,
+          "rotation": 0,
+          "target": "1-frente-da-igreja"
+        },
+        {
+          "yaw": -1.8759085816787557,
+          "pitch": -0.11153583303096681,
+          "rotation": 0,
+          "target": "4-praa-alencastro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.752484195135949,
+          "pitch": 0.12961065690972795,
+          "title": "Praça da República",
+          "text": "A Praça da República, em frente à Catedral de Cuiabá, é um dos espaços mais simbólicos da cidade.Conhecida como Centro do Poder, já foi palco de disputas políticas no século XIX, hoje abriga marcos históricos como o Palácio da Instrução, o Museu Histórico de Mato Grosso e o edifício dos Correios, reunindo memória, arquitetura e cultura no coração da capital."
+        },
+        {
+          "yaw": 2.0247096472703063,
+          "pitch": -0.13634033718419758,
+          "title": "Correios",
+          "text": "Constuído em 1937, a agência dos Correios da Praça da República é um marco da arquitetura art déco em Cuiabá, sendo o primeiro edifício da cidade a adotar esse estilo. Com suas linhas retas, formas abstratas e concreto armado, destaca-se em meio às construções coloniais do centro histórico. O prédio reflete um capítulo importante da história arquitetônica do Brasil, resultado da política varguista de padronização das agências postais em todo o país."
+        },
+        {
+          "yaw": -0.7986095650063589,
+          "pitch": -0.0729631702023461,
+          "title": "Monumento dos Direitos Humanos Universais",
+          "text": "O Monumento aos Direitos Humanos, situado na Praça da República, foi inaugurado em 1998 para celebrar os 50 anos da Declaração Universal dos Direitos Humanos. Criado por Jonas Correia Neto, retrata a deusa Têmis, símbolo da Justiça, com uma balança desequilibrada e figuras que representam injustiça, ganância e indiferença social, lembrando que os direitos humanos devem ser protegidos em todo o país. Tombado como patrimônio histórico em 1999, o monumento é um marco de reflexão e memória para Cuiabá."
+        }
+      ]
+    },
+    {
+      "id": "1-frente-da-igreja",
+      "name": "Frente da Igreja",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2723.5,
+      "initialViewParameters": {
+        "yaw": 0.3380316680968729,
+        "pitch": -0.28118489329035157,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7837425568518572,
+          "pitch": -0.17927506605821364,
+          "rotation": 0,
+          "target": "3-palcio-da-instruo"
+        },
+        {
+          "yaw": 0.9180730192769531,
+          "pitch": -0.1497944121813024,
+          "rotation": 0,
+          "target": "4-praa-alencastro"
+        },
+        {
+          "yaw": 0.16002763200967252,
+          "pitch": -0.3863867501885849,
+          "rotation": 0,
+          "target": "2-igreja-matriz---interior"
+        },
+        {
+          "yaw": 2.584617025162359,
+          "pitch": -0.16802181324784904,
+          "rotation": 0,
+          "target": "0-praa-da-repblica"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.18430087390165,
+          "pitch": -0.08217257741544692,
+          "title": "Feira do Artesanato (Feirinha da Matriz)",
+          "text": "Com mais de 25 anos de tradição, a Feira do Artesanato é um dos espaços culturais mais marcantes de Cuiabá. Localizada na viela entre o Palácio da Instrução e a Igreja Matriz, em frente à Praça da República, ela reúne diariamente artesãos que expõem trabalhos típicos e criativos. O público pode circular até a saída pela Avenida Joaquim Murtinho, vivenciando de perto a arte e a cultura cuiabana."
+        }
+      ]
+    },
+    {
+      "id": "2-igreja-matriz---interior",
+      "name": "Igreja Matriz - Interior",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2648,
+      "initialViewParameters": {
+        "yaw": -2.5268733058861095,
+        "pitch": -0.25699735652998257,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06125197162840834,
+          "pitch": -0.010577018854680986,
+          "rotation": 0,
+          "target": "1-frente-da-igreja"
+        },
+        {
+          "yaw": -1.979030474628786,
+          "pitch": -0.07575503258405725,
+          "rotation": 0,
+          "target": "4-praa-alencastro"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.788099262848669,
+          "pitch": -0.27710089860418385,
+          "title": "Igreja Matriz",
+          "text": "A Catedral Metropolitana de Cuiabá, dedicada ao Senhor Bom Jesus, é um marco histórico da cidade desde o século XVIII. A igreja passou por várias reconstruções, evoluindo de uma capela de pau-a-pique a uma imponente catedral neoclássica, tornando-se sede da Diocese de Cuiabá em 1826. Seu subsolo abriga uma cripta com restos de figuras históricas, e hoje a catedral é um dos principais pontos turísticos e símbolos religiosos e culturais da capital."
+        }
+      ]
+    },
+    {
+      "id": "3-palcio-da-instruo",
+      "name": "Palácio da Instrução",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.7375403753063416,
+        "pitch": -0.26850415366088143,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14879479722429068,
+          "pitch": -0.05876299187368872,
+          "rotation": 0,
+          "target": "0-praa-da-repblica"
+        },
+        {
+          "yaw": -1.522775645684428,
+          "pitch": -0.1521092741691774,
+          "rotation": 0,
+          "target": "1-frente-da-igreja"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6254731876708863,
+          "pitch": -0.3487711407192755,
+          "title": "Palácio da Instrução",
+          "text": "O Palácio da Instrução é um dos marcos históricos de Cuiabá, inaugurado em 1914. Tombado como patrimônio estadual em 1983, já abrigou importantes instituições de ensino, como a Escola Normal Pedro Celestino e o Liceu Cuiabano. Ao longo do tempo, também sediou secretarias estaduais, a Biblioteca Estevão de Mendonça, museus e a Pinacoteca. Após sua revitalização em 2004, o prédio passou a receber grandes mostras culturais e exposições de arte."
+        },
+        {
+          "yaw": 1.2000190861867104,
+          "pitch": -0.28617654602493126,
+          "title": "Thesouro do Estado",
+          "text": "O Thesouro do Estado, construído em 1896, é um dos mais importantes edifícios históricos de Cuiabá. Localizado na Praça da República, já foi sede do tesouro estadual e de diversas repartições públicas. Tombado em 1983, o prédio em estilo neoclássico passou a abrigar, em 2006, o Museu Histórico de Mato Grosso, preservando e expondo a memória política e cultural do Estado."
+        }
+      ]
+    },
+    {
+      "id": "4-praa-alencastro",
+      "name": "Praça Alencastro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0588912128395567,
+        "pitch": 0.0041947832750999225,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.017804571578756,
+          "pitch": -0.041122484738606246,
+          "rotation": 0,
+          "target": "1-frente-da-igreja"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5887186851735855,
+          "pitch": 0.07570838236907562,
+          "title": "Praça Alencastro",
+          "text": "<p data-start=\"211\" data-end=\"571\">Originalmente chamada <strong data-start=\"298\" data-end=\"318\">Largo do Palácio</strong>, no início do século XIX foi palco de festas, touradas e cerimônias religiosas.</p>\n<p data-start=\"573\" data-end=\"886\">O espaço ganhou o nome atual em homenagem aos governadores da família Alencastro, responsáveis pela criação do <strong data-start=\"684\" data-end=\"721\">primeiro jardim público de Cuiabá</strong>, inaugurado em 1882. A praça recebeu coreto, chafariz (que não se encontra mais no local), iluminação moderna e tornou-se ponto de encontro da população, com festejos musicais e intensa vida social.</p>"
+        },
+        {
+          "yaw": -1.1138642983175835,
+          "pitch": -0.26515910463654535,
+          "title": "Palácio Alencastro (Sede da Prefeitura de Cuiabá)",
+          "text": "Construído em 1959 sobre os escombros do antigo Palácio do Governo, demolido em meio ao ciclo desenvolvimentista de Juscelino Kubitschek, o Palácio Alencastro marcou a introdução da arquitetura modernista em Cuiabá."
+        },
+        {
+          "yaw": -0.1901879068279797,
+          "pitch": -0.3566188819213423,
+          "title": "Edifício Maria Joaquina",
+          "text": "Inaugurado em 8 de abril de 1969, dia do aniversário da Capital, o prédio era moradia da alta sociedade cuiabana.&nbsp;"
+        },
+        {
+          "yaw": -1.977935961117236,
+          "pitch": 0.0306178092436582,
+          "title": "Busto das Personalidades",
+          "text": "Em referência a estas importantes figuras políticas mato-grossenses, os bustos foram produzidos a partir de uma mistura de cimento com fibra e pintados com tinta de coloração que imita o bronze.&nbsp;"
+        },
+        {
+          "yaw": -0.08628843654337892,
+          "pitch": -0.13921479124941882,
+          "title": "Coreto Praça Alencastro",
+          "text": "O coreto da Praça Alencastro é o <strong data-start=\"128\" data-end=\"159\">segundo construído no local</strong>. O primeiro, instalado no início do século XX, foi transferido para a Praça Ipiranga por volta de 1930. Desde então, o atual coreto se tornou palco de encontros, festivais musicais e momentos culturais que marcaram a vida social de Cuiabá."
+        }
+      ]
+    }
+  ],
+  "name": "Centro Cuiabá",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
